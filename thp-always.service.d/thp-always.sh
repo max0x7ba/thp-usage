c2da6281e15699d55632cc3d2730ec6b7fa17415
Maxim Egorushkin <maxim.egorushkin@gmail.com>
2023-02-07 01:40:19 +0000

    Maximize max_ptes_shared.

--- thp-always.service.d/thp-always.sh
+++ thp-always.service.d/thp-always.sh
@@ -9,3 +9,6 @@ echo always > /sys/kernel/mm/transparent_hugepage/enabled
 echo defer+madvise > /sys/kernel/mm/transparent_hugepage/defrag
 echo 0 > /sys/kernel/mm/transparent_hugepage/khugepaged/max_ptes_none
 echo 1 > /proc/sys/vm/overcommit_memory
+
+max_ptes_shared=/sys/kernel/mm/transparent_hugepage/khugepaged/max_ptes_shared
+echo $(($(cat $max_ptes_shared) * 2 - 1)) > $max_ptes_shared
