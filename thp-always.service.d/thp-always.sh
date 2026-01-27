9bd51a0ed6cb7595a4bdf66b4175a16b7173da18
Maxim Egorushkin <maxim.egorushkin@gmail.com>
2026-01-27 19:02:35 +0000

    Always defrag.

--- thp-always.service.d/thp-always.sh
+++ thp-always.service.d/thp-always.sh
@@ -5,10 +5,15 @@
 #     cat $f
 # done
 
-echo always > /sys/kernel/mm/transparent_hugepage/enabled
-echo defer+madvise > /sys/kernel/mm/transparent_hugepage/defrag
-# echo 0 > /sys/kernel/mm/transparent_hugepage/khugepaged/max_ptes_none
 echo 1 > /proc/sys/vm/overcommit_memory
+
+echo always > /sys/kernel/mm/transparent_hugepage/enabled
+# echo defer+madvise > /sys/kernel/mm/transparent_hugepage/defrag
+echo always > /sys/kernel/mm/transparent_hugepage/defrag
+
+echo 64 > /sys/kernel/mm/transparent_hugepage/khugepaged/max_ptes_none
+echo 64 > /sys/kernel/mm/transparent_hugepage/khugepaged/max_ptes_shared
+echo 0 > /sys/kernel/mm/transparent_hugepage/khugepaged/max_ptes_swap
 
 # readonly max_ptes_shared_original=/run/max_ptes_shared.original.txt
 # if [[ ! -f $max_ptes_shared_original ]]; then
