5439a3545a78633e1c7b30a26e5af204efbfb242
Maxim Egorushkin <maxim.egorushkin@gmail.com>
2026-03-27 09:35:43 +0000

    Default to short summary benchmark results.

--- thp-benchmark.sh
+++ thp-benchmark.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Required Ubuntu packages:
-# sudo apt install coreutils sed stress-ng icdiff
+# sudo apt install coreutils sed awk stress-ng icdiff
 
 set -e
 
@@ -9,10 +9,9 @@ readonly repo_dir="$(dirname "$(realpath "$BASH_ARGV0")")"
 cd "$repo_dir"
 
 readonly cR="\e[31m" cG="\e[32m" cY="\e[33m" cB="\e[34m" cM="\e[35m" cC="\e[36m" cD="\e[0m"
-function log { printf "%(%F %T)T %b${cD}\n" -1 "$1"; }
-function log2 { printf "\n%(%F %T)T %b${cD}\n" -1 "$1"; }
-
-readonly thp_cfg_dir=/sys/kernel/mm/transparent_hugepage
+function log {
+    printf "%(%F %T)T %b${cD}\n" -1 "$1";
+}
 
 readonly thp_cfg_default=thp-always.service.d/thp-default.sh
 readonly thp_cfg_always=thp-always.service.d/thp-always.sh
@@ -25,19 +24,47 @@ function load-thp-option-value {
 }
 
 function load-thp-cfg {
+    local thp_parameter_dir=/sys/kernel/mm/transparent_hugepage
     local value thp_opt_path thp_opt
     for thp_opt in enabled defrag khugepaged/{max_ptes_none,max_ptes_shared,max_ptes_swap,pages_to_scan,scan_sleep_millisecs}; do
-        thp_opt_path="$thp_cfg_dir/$thp_opt"
+        thp_opt_path="$thp_parameter_dir/$thp_opt"
         load-thp-option-value
         printf "echo %8s > $thp_opt_path\n" "$value"
     done
 }
 
+function bogo-ops-speedup {
+    awk -f- "$@" <<"EOF"
+/^matrix([[:space:]]+[[:digit:].]+)+/ && NF==9 {
+    bogo_ops[file_idx++]=$6
+}
+END {
+    bogo_ops_speedup_pct = ((bogo_ops[1] - bogo_ops[0]) / bogo_ops[0]) * 100
+    printf "%+6.1f%% (before %10.2f, after %10.2f)\n", bogo_ops_speedup_pct, bogo_ops[0], bogo_ops[1]
+}
+EOF
+}
+
+function get-best-sched-opts {
+    local -n opts=$1
+    opts="chrt --fifo 10"
+    $opts true || opts="sudo $opts"
+    $opts true || opts="chrt --batch 0 nice -n-20"
+    $opts true || opts="nice -n-20"
+    $opts true || opts=""
+}
+
+function exit-on-signal {
+    trap "log \"${cR}received $1, terminating.\"; exit 1" $1
+}
+
 # ~/src/thp-usage/thp-benchmark.sh
-# n_cpus=2 n_ops=100 ~/src/thp-usage/thp-benchmark.sh
+# v=1 n_cpus=2 n_ops=100 ~/src/thp-usage/thp-benchmark.sh
 function thp-benchmark {(
     local -i n_cpus=${n_cpus:-1}
-    local -i n_ops=${n_ops:-2000}
+    local -i n_ops=${n_ops:-1000}
+    local -i size=${size:-2048}
+    local -i v=${v:-0}
 
     local log_dir=${log_dir:-/tmp/thp-benchmark}
     mkdir -p $log_dir
@@ -45,35 +72,55 @@ function thp-benchmark {(
     local -i cpu_step=${cpu_step:-2}
     local cpu_list=${cpu_list:-"$(seq --separator=, 0 $cpu_step $((n_cpus * cpu_step - 1)))"}
 
-    local sched_opts="chrt --fifo 10"
-    $sched_opts true || sched_opts="sudo $sched_opts"
-    $sched_opts true || sched_opts="chrt --batch 0 nice -n-20"
-    $sched_opts true || sched_opts="nice -n-20"
-    $sched_opts true || sched_opts=""
+    local sched_opts
+    get-best-sched-opts sched_opts
+
+    # --seed 0 makes the test reproducible.
+    # --no-madvise disables the random advise options.
+    local args="--matrix $n_cpus --taskset $cpu_list --matrix-ops $n_ops --matrix-size $size --no-madvise --seed 0 --metrics --perf"
+
+    # stress-ng doesn't fail on signals.
+    exit-on-signal SIGINT
+    exit-on-signal SIGTERM
 
     local methods="copy negate mult add mean"
     for thp_cfg in default always; do
         local -n cfg_path=thp_cfg_${thp_cfg}
-        log2 "${cY}Benchmark ${cG}${thp_cfg}${cY} THP settings with $n_cpus CPUs ($cpu_list) for $n_ops operations."
+        log "${cY}Apply ${cG}${thp_cfg}${cY} THP settings."
         sed -E '/^echo/!d; s/ +/ /g' ${cfg_path}
-        sudo bash -c "source ${cfg_path}"
         echo
+        sudo bash -c "source ${cfg_path}"
 
+        log "${cY}Benchmark with ${cG}${thp_cfg}${cY} THP settings."
         for method in $methods; do
-            cmd="${sched_opts} stress-ng --matrix $n_cpus --taskset $cpu_list --matrix-method $method --matrix-ops $n_ops --matrix-size 1024 --no-madvise --seed 0 --metrics-brief --perf"
+            printf -v cmd "%s stress-ng --matrix-method %-6s %s" "$sched_opts" $method "$args"
             log "$cmd"
             $cmd > $log_dir/${thp_cfg}.${method}.log
         done
+        echo
     done
 
-    log2 "${cY}Compare benchmark results."
+    # Strip off stress-ng log line prefixes with pids.
     for method in $methods; do
         sed -i.orig.log -E 's/^[^]]+\] //; /^(skipped|passed|failed|metrics untrustworthy):/d' $log_dir/{default,always}.${method}.log
-        echo; icdiff --cols 200 --no-bold --show-no-spaces $log_dir/{default,always}.${method}.log | tee $log_dir/result.${method}.txt
     done
+
+    log "${cY}Benchmark results summary, bogo ops/s:"
+    for method in $methods; do
+        printf "%-6s: %s\n" $method "$(bogo-ops-speedup $log_dir/{default,always}.${method}.log)"
+    done
+    echo
+
+    if((v>=1)); then
+        log "${cY}Benchmark metrics comparison."
+        for method in $methods; do
+            icdiff --cols 200 --no-bold --show-no-spaces $log_dir/{default,always}.${method}.log | tee $log_dir/result.${method}.txt
+            echo
+        done
+    fi
 )}
 
-# Save default TCP configuration.
+# Save default THP configuration, if missing.
 [[ -s $thp_cfg_default ]] || { load-thp-cfg > $thp_cfg_default; }
 
 thp-benchmark
